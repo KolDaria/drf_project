a4a8b0a03e385c2de7472b091e0ac5e0f44e87c2
Daria Kolisnyk <dashamel17@gmail.com>
2025-07-08 21:07:31 +0300

    Описание CRUD для представлений

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('', include('materials.urls', namespace='materials')),
 ]
 
 if settings.DEBUG:
