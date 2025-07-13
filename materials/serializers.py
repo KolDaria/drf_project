7d693336b36a6f0943af1f66f463be7e516f8abe
Daria Kolisnyk <dashamel17@gmail.com>
2025-07-13 13:11:55 +0300

    Реализация сериализаторов

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,14 +3,20 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    number_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def get_number_lessons(self, instance):
+        return instance.lessons.count()
---
+++ users/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+from users.models import Payments
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'phone', 'country', 'avatar', 'is_blocked')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class PaymentsSerializer(serializers.ModelSerializer):
+    user_email = serializers.CharField(source='user.email', read_only=True)
+    paid_course_name = serializers.CharField(source='paid_course.name', read_only=True, default=None)
+    paid_lesson_name = serializers.CharField(source='paid_lesson.name', read_only=True, default=None)
+
+    class Meta:
+        model = Payments
+        fields = ['id', 'user', 'user_email', 'payment_date', 'paid_course', 'paid_course_name', 'paid_lesson',
+                  'paid_lesson_name', 'payment_amount', 'payment_method']
