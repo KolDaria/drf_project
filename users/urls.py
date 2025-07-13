74c92bd57c6589d22534ce13677de583620ff1d5
Daria Kolisnyk <dashamel17@gmail.com>
2025-07-13 13:12:51 +0300

    Настроены пути, представления

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import UserCreate, PaymentListAPIView
 
 app_name = UsersConfig.name
 
-urlpatterns = [
 
+urlpatterns = [
+    path('users/',UserCreate.as_view(), name='user-create'),
+    path('payments/',PaymentListAPIView.as_view(), name='payment-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.models import Payments
+from users.serializers import UserSerializer, PaymentsSerializer
+
+User = get_user_model()
+
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ['payment_date', 'payment_amount']
+    filterset_fields = ['paid_course', 'paid_lesson', 'payment_method', 'user']
