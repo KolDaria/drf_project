4e405e318ae336738df5e430b5fe5fa0d289a33c
Daria Kolisnyk <dashamel17@gmail.com>
2025-07-22 13:45:29 +0300

    Реализована доп. проверка на сторонние ресурсы

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from materials.models import Course, Lesson
+from materials.validators import TitleValidator
+from users.models import Subscription
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -9,11 +11,16 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [
+            TitleValidator(field='video_url'),
+            serializers.UniqueTogetherValidator(fields=['video_url'], queryset=Lesson.objects.all())
+        ]
 
 
 class CourseSerializer(serializers.ModelSerializer):
     number_lessons = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
@@ -21,3 +28,9 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_number_lessons(self, instance):
         return instance.lessons.count()
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Subscription.objects.filter(user=user, course=obj).exists()
+        return False
---
+++ materials/validators.py
@@ -0,0 +1,14 @@
+import re
+
+from rest_framework.exceptions import ValidationError
+
+
+class TitleValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        reg = re.compile(r'https?://(www\.)?youtube\.com/.*')
+        tmp_val = dict(value).get(self.field)
+        if not bool(reg.match(tmp_val)):
+            raise ValidationError('Сторонний ресурс')
